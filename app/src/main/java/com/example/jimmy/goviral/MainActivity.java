5f94db45f15880310475cd20269515f4c496a993
Jimmy <min1jim@hotmail.com>
2016-04-18 14:16:55 +0800

    Added Information Activity

--- app/src/main/java/com/example/jimmy/goviral/MainActivity.java
+++ app/src/main/java/com/example/jimmy/goviral/MainActivity.java
@@ -6,7 +6,10 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.android.youtube.player.YouTubePlayer.Provider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends YouTubeBaseActivity implements
@@ -14,14 +17,28 @@ public class MainActivity extends YouTubeBaseActivity implements
 
     public static final String API_KEY = "AIzaSyCe6tORd9Ch4lx-9Ku5SQ476uS9OtZYsWA";
     public static final String VIDEO_ID = "o7VVHhK9zf0";
+    private static Button show_stat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        YouTubePlayerView youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubeplayerview);
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtube_player);
         youTubePlayerView.initialize(API_KEY, this);
+    }
+
+    public void OnClickButtonListener(){
+        show_stat = (Button)findViewById(R.id.button_info);
+        show_stat.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.jimmy.goviral.information");
+                        startActivityForResult(intent, 1);
+                    }
+                }
+        );
     }
 
     @Override
---
+++ app/src/main/java/com/example/jimmy/goviral/information.java
@@ -0,0 +1,14 @@
+package com.example.jimmy.goviral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_information);
+    }
+}
